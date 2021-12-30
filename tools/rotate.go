6e31bdeed58608b40bf0d0d2f4785f16e4aeea31
Andrew <andrewnijmeh1@gmail.com>
2021-12-30 11:35:29 -0500

    finish this func

--- tools/rotate.go
+++ tools/rotate.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"os/user"
+	"testing/quick"
 )
 
 func LogMustRotate(logfile string, maxSize int64) (bool, error) {
@@ -35,7 +36,7 @@ func Rotate(filename string, tmpFilename string) (int64, error) {
 	exists, err := tmpFileExists(tmp)
 
 	if err != nil {
-		return 0, fmt.Errorf("Erorr when checking temp log: %s", err.Error())
+		return 0, fmt.Errorf("Error when checking temp log: %s")
 	}
 
 	if exists {
@@ -53,6 +54,44 @@ func Rotate(filename string, tmpFilename string) (int64, error) {
 				return 0, fmt.Errorf("failed to clean temp log")
 			}
 		}
-	}
+		
+		sourceFileStat, err := os.Stat(src)
 
+		if err != nil {
+						return 0, fmt.Errorf("failed to stat log file")
+		}
+
+		if !sourceFileStat.Mode().IsRegular() {
+						return 0, fmt.Errorf("%s is not regular file", src)
+		}
+
+		source, err := os.Open(src)
+
+		if err != nil {
+						return 0, err
+		}
+
+		defer source.Close()
+
+		dst, err := os.OpenFile(tmp, os.O_APPEND|os.O_CREATE|os.O_WRONGLY, 0644)
+
+		if err != nil {
+						return 0, fmt.Errorf("failed to open %s temporary log file", tmp)
+		}
+
+		defer dst.Close()
+
+		nBytes, err := io.Copy(dst, source)
+
+		if err != nil {
+				return 0, fmt.Errorf("Failed to copy log to temporary log file.")
+		}
+
+		_, err = cleanFile(src)
+
+		if err != nil {
+				return 0, fmt.Errorf("Failed to clean snitched log file")			
+		}
+
+		return nBytes, err
 }
